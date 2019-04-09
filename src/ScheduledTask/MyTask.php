3902d759e0876032866df7c57de9c37362fd336a
Patrick Stahl <p.stahl@shopware.com>
2019-04-09 09:28:44 +0200

    Switch to new structure

--- src/ScheduledTask/MyTask.php
+++ src/ScheduledTask/MyTask.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace ScheduledTaskPlugin\ScheduledTask;
+namespace Swag\ScheduledTaskPlugin\ScheduledTask;
 
 use Shopware\Core\Framework\ScheduledTask\ScheduledTask;
 
---
+++ src/ScheduledTaskPlugin.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Swag\ScheduledTaskPlugin;
+
+use Shopware\Core\Framework\Plugin;
+
+class ScheduledTaskPlugin extends Plugin
+{
+}
